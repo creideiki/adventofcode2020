5a37ba466a09e404e8f7bdc5aca8d9eb74944ed6
Karl-Johan Karlsson <creideiki@ferretporn.se>
2020-12-15 11:05:07 +0100

    Day 15 part 1 solutions

---
+++ 15-1.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+File.read('15.input').lines.map(&:strip).each do |problem|
+  seen = Hash.new([])
+  numbers = problem.split(',').map(&:to_i)
+  numbers.each_index do |pos|
+    seen[numbers[pos]] = [pos] + seen[numbers[pos]]
+  end
+  numbers.size.upto 2019 do |pos|
+    if seen[numbers[pos - 1]].size == 1
+      numbers.push 0
+      seen[0] = [pos] + seen[0][..0]
+    else
+      age = seen[numbers[pos - 1]][0] - seen[numbers[pos - 1]][1]
+      numbers.push age
+      seen[age] = [pos] + seen[age][..0]
+    end
+  end
+  print numbers.last, "\n"
+end
